419654feb232b932aab5b4879dcb75bc5cba30ee
chenyanai <chenyanai@gmail.com>
2020-05-27 16:50:24 +0300

    Fixed alot of bugs in the preprocessing

--- Model.py
+++ Model.py
@@ -1,10 +1,11 @@
 from tensorflow.keras.layers import Flatten, Embedding, Dense, GRU, TimeDistributed, LSTM, Dropout, Concatenate
 from tensorflow.keras.callbacks import ModelCheckpoint
-# from tensorflow.keras.layers import TensorBoard
+from tensorflow.keras.callbacks import TensorBoard
 from tensorflow.keras import Model, Input
-from tensorflow.keras.utils import to_categorical
 import numpy as np
 from Preprocessing import one_hot_by_max_value
+from datetime import datetime
+from sklearn.preprocessing import normalize
 
 def build_model(sequence_length, mid_data_len, embedding_matrix, vocab_size):
 
@@ -18,7 +19,7 @@ def build_model(sequence_length, mid_data_len, embedding_matrix, vocab_size):
 
     lyrics_melody_concat = Concatenate(axis=-1, name='lyrics_melody_concat')([lyrics_flatten, melody_lstm])
 
-    dense = Dense(1024, activation='relu', name='first_dense')(lyrics_melody_concat)
+    dense = Dense(500, activation='relu', name='first_dense')(lyrics_melody_concat)
     dropout = Dropout(0.1)(dense)
     dense = Dense(vocab_size + 1, activation='softmax', name='last_dense')(dropout) # Predicting the next locations of the defense players
 
@@ -29,9 +30,24 @@ def build_model(sequence_length, mid_data_len, embedding_matrix, vocab_size):
 
 
 def train_model(model, X, y, vocab_size):
-    # X_melody = np.vstack(X['melody_vectors'])
+
+    log_dir = "Logs/" + datetime.now().strftime("%Y%m%d-%H%M%S")
+    tensorboard_callback = TensorBoard(log_dir=log_dir, histogram_freq=1)
+
     X_lyrics = np.vstack(X['lyrics'])
     y = one_hot_by_max_value(y, vocab_size)
-    # y_binary = to_categorical(y)
-    model.fit(x=[X['melody_vectors'], X_lyrics], y=y, batch_size=256, epochs=3, verbose=1)
+
+    X_melody = []
+    for array in X['melody_vectors']:
+        X_melody.append(normalize(array))
+    X_melody = np.array(X_melody)
+    batch_size = 16
+    model.fit(x=[X_melody, X_lyrics],
+              y=y,
+              batch_size=batch_size,
+              epochs=3,
+              verbose=1,
+              steps_per_epoch=int(len(X_lyrics) / batch_size))
+              # callbacks=[tensorboard_callback])
+    model.save("model.h5")
     return model
--- Preprocessing.py
+++ Preprocessing.py
@@ -2,6 +2,7 @@ import pandas as pd
 import pretty_midi
 from tqdm import tqdm
 from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.utils import to_categorical
 import os
 import re
 import numpy as np
@@ -89,21 +90,23 @@ def clean_text(text:str)->str:
 def read_midi_files(path, train_index, test_index, vocab_size):
     train_midi_dict = {}
     test_midi_dict = {}
-
+    testing = 5
     for file in tqdm(os.listdir(path)):
         file_path = os.path.join(path, file)
         song_name = get_song_name_from_file_name(file)
         if song_name in train_index['song_name'].values:
             try:
-                pm = pretty_midi.PrettyMIDI(file_path)
-                song_lyrics = train_index[train_index['song_name'] == song_name]['lyrics_sequence'].values[0]
+                if testing > 0:
+                    pm = pretty_midi.PrettyMIDI(file_path)
+                    song_lyrics = train_index[train_index['song_name'] == song_name]['lyrics_sequence'].values[0]
 
-                # one_hot_lyrics = np.zeros((len(song_lyrics), vocab_size + 1))
-                # one_hot_lyrics[np.arange(len(song_lyrics)), song_lyrics] = 1
+                    # one_hot_lyrics = np.zeros((len(song_lyrics), vocab_size + 1))
+                    # one_hot_lyrics[np.arange(len(song_lyrics)), song_lyrics] = 1
 
-                # song_lyrics = tokenizer.texts_to_matrix(one_hot_lyrics)
+                    # song_lyrics = tokenizer.texts_to_matrix(one_hot_lyrics)
 
-                train_midi_dict[file[:-4]] = [pm, song_lyrics]
+                    train_midi_dict[file[:-4]] = [pm, song_lyrics]
+                    testing -= 1
             except:
                 print(file)
         elif song_name in test_index['song_name'].values:
@@ -121,8 +124,9 @@ def read_midi_files(path, train_index, test_index, vocab_size):
 
 
 def one_hot_by_max_value(data, vocab_size):
-    one_hot_array = np.zeros((len(data), vocab_size + 1))
-    one_hot_array[np.arange(len(data)), data] = 1
+    one_hot_array = to_categorical(data, num_classes=vocab_size + 1)
+    # one_hot_array = np.zeros((len(data), vocab_size + 1))
+    # one_hot_array[np.arange(len(data)), data] = 1
     return one_hot_array
 
 # def convert_lyrics_to_one_hot_array(lyrics, tokenizer):
@@ -160,9 +164,13 @@ def convert_data_to_model_input(df, sequence_length):
 
         if len(melody_data) >= sequence_length + 1:
             for j in range(len(melody_data)):
-                if len(melody_data) - 1 > j + sequence_length :
-                    X['lyrics'].append(song_data['lyrics'][j])
-                    X['melody_vectors'].append(song_data['chroma_vectors'][j:j + sequence_length])
-                    y.append(song_data['lyrics'][j + sequence_length])
+                if len(melody_data) > j + sequence_length + 1:
+                    if len(song_data['lyrics']) > j + sequence_length + 1:
+                        try:
+                            X['lyrics'].append(song_data['lyrics'][j + sequence_length])
+                            X['melody_vectors'].append(song_data['chroma_vectors'][j:j + sequence_length])
+                            y.append(song_data['lyrics'][j + sequence_length + 1])
+                        except:
+                            print('hi')
     y = np.vstack(y)
     return X, y
